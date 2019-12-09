2160f1dcce64775332b886812bd81a8e8e14461c
ledat91 <datlk@rikkeisoft.com>
2019-12-09 14:44:54 +0700

    datlk/complete-dev

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i < 10; $i++) {
+            factory(\App\User::class)->create([
+                'name' => "test$i"
+            ]);
+        }
+    }
+}
